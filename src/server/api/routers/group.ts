fba6d077c69df13550ceab8826d3276f8827acc6
Jun Xu <63337934+gekjunxu@users.noreply.github.com>
2026-06-16 19:19:59 +0900

    Update group.ts try again

--- src/server/api/routers/group.ts
+++ src/server/api/routers/group.ts
@@ -280,10 +280,12 @@ export const groupRouter = createTRPCRouter({
 const totalsByUser = expenses.reduce<Record<number, Record<string, bigint>>>(
       (acc, expense) => {
         expense.expenseParticipants.forEach((participant) => {
-          const attributedAmount =
-            participant.userId === expense.paidBy
-              ? expense.amount - participant.amount
-              : -participant.amount;
+const attributedAmount =
+  participant.userId === expense.paidBy
+    ? expense.expenseParticipants.length === 1
+      ? participant.amount
+      : expense.amount - participant.amount
+    : -participant.amount;
 
           acc[participant.userId] ??= {};
           acc[participant.userId]![expense.currency] =
